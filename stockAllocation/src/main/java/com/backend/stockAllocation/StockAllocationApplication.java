f50f90a953ca74e0c0e5a814728380e743cb3bc2
Malaya Sahu <malaya.sahu@bounteous.com>
2026-04-14 16:01:55 +0530

    first commit

---
+++ stockAllocation/src/main/java/com/backend/stockAllocation/StockAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.backend.stockAllocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAllocationApplication.class, args);
+	}
+
+}
---
+++ stockAllocation/src/test/java/com/backend/stockAllocation/StockAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.stockAllocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
